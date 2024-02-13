423381b195d80dfaf5f19c7e1db2c7059a974727
Lance Burk <burk.lance.b@gmail.com>
2024-02-13 15:06:40 -0600

    test test

---
+++ Excersizes/D-jango/6django/5django_rest/books/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# from django.views.generic import 
+# from .models import Book  # Import your Book model
+
+class BookListView():
+    # model = book_list  
+    template_name = book_list
